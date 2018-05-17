0e5f30bb1fe6f3ba5cff1bea4b70cbe85d182ba0
m-przybylski <m-przybylski@hotmail.com>
2018-05-17 20:47:42 +0200

    guards and redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,44 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { SearchComponent } from './UI/search/search.component';
 import { PlayerComponent } from './UI/player/player.component';
+import { LoginComponent } from './UI/login/login.component';
+import { PasswordComponent } from './UI/password/password.component';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
-  { path: '', component: SearchComponent },
-  { path: ':videoId', component: PlayerComponent }
+  {
+    path: '',
+    component: SearchComponent,
+    canActivate: ['canActivateUser'],
+    pathMatch: 'full'
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'login/:userName', component: PasswordComponent },
+  {
+    path: ':videoId',
+    component: PlayerComponent,
+    canActivate: ['canActivateUser']
+  }
 ];
 
+const redirecter = (auth: AuthService, router: Router) => () => {
+  if (auth.isAuth) {
+    return auth.isAuth;
+  } else {
+    router.navigate(['/login']);
+  }
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  providers: [
+    {
+      provide: 'canActivateUser',
+      useFactory: redirecter,
+      deps: [AuthService, Router]
+    }
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,4 +3,12 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {}
+export class AuthService {
+  private _isAuthenticated = false;
+  public get isAuth(): boolean {
+    return this._isAuthenticated;
+  }
+  public set isAuth(v: boolean) {
+    this._isAuthenticated = v;
+  }
+}
