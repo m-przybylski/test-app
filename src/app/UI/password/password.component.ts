3d0e90cb2e4daabcab1c89ccb5675389fe9c9d7e
m-przybylski <m-przybylski@hotmail.com>
2018-05-17 20:59:31 +0200

    dummy authentication

--- src/app/UI/password/password.component.ts
+++ src/app/UI/password/password.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'byt-password',
   templateUrl: './password.component.html',
   styles: []
 })
-export class PasswordComponent implements OnInit {
+export class PasswordComponent {
+  constructor(private router: Router, private auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  login(password: string) {
+    this.auth.isAuth = true;
+    this.router.navigate(['/']);
   }
-
 }
